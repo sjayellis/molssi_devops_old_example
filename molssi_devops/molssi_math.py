4fd060af7bda52bca439ee77a000bb302d576227
Samuel Ellis <sjellis@vt.edu>
2019-05-12 21:29:14 -0400

    Added a mean function to molssi_math

--- molssi_devops/molssi_math.py
+++ molssi_devops/molssi_math.py
@@ -28,6 +28,29 @@ def canvas(with_attribution=True):
         quote += "\n\t- Adapted from Henry David Thoreau"
     return quote
 
+def mean(num_list):
+    """
+    Calculate the mean/average of a list of numbers.
+
+    Parameters
+    ----------
+    num_list : list
+        The list to take the average of
+
+    Returns
+    -------
+    mean_list : float
+        The mean of the list
+
+    Examples
+    --------
+    >>> mean([1, 2, 3, 4, 5])
+    3.0
+    """
+
+    mean_list = sum(num_list)/len(num_list)
+    return mean_list
+
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
